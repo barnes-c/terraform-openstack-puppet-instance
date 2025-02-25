ad2cc0c2cddf33aed044a39a14cf879aaf97eb96
Christopher Barnes <christopher.barnes@cern.ch>
2025-02-25 20:01:46 +0100

    Add required vars to foreman host

--- foremen.tf
+++ foremen.tf
@@ -1,18 +1,37 @@
-resource "null_resource" "foreman_configuration" {
-  provisioner "local-exec" {
-    command = "echo 'Setting environment variables'"
-    environment = {
-      KRB5CCNAME  = "FILE:/run/user/107352/krb5cc"
-      KRB5_CONFIG = "./krb5.conf"
-    }
-  }
+resource "foreman_host" "host" {
+  architecture_id    = resource.foreman_architecture.architecture.id
+  operatingsystem_id = data.foreman_operatingsystem.os.id
+  domain_id          = resource.foreman_domain.domain.id
+  # ptable_id = 1
+  # medium_id
+  # full_messages
+  name           = var.instance_name
+  hostgroup_id   = data.foreman_hostgroup.hostgroup.id
+  environment_id = data.foreman_environment.env.id
+
+  # puppet_class_ids = [data.foreman_puppetclass.class.id]
 }
+
+# data "foreman_puppetclass" "class" {
+#   name = "qa"
+# }
 
 data "foreman_hostgroup" "hostgroup" {
   title = var.hostgroup
 }
 
-output "hostgroup" {
-  value = data.foreman_hostgroup.hostgroup
+data "foreman_environment" "env" {
+  name = var.foreman_environment
 }
 
+resource "foreman_architecture" "architecture" {
+  name = var.arch
+}
+
+data "foreman_operatingsystem" "os" {
+  title = format("%s %s.%s", var.distro, var.os_major_version, var.os_minor_version)
+}
+
+resource "foreman_domain" "domain" {
+  name = "${var.instance_name}.cern.ch"
+}
