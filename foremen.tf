3b53ba4874ae0254ac9d13e9de4418ce385cd1f3
Christopher Barnes <christopher.barnes@cern.ch>
2025-02-26 15:40:45 +0100

    Sort attributes ascending

--- foremen.tf
+++ foremen.tf
@@ -1,27 +1,27 @@
 resource "foreman_host" "host" {
-  architecture_id    = data.foreman_architecture.architecture.id
-  operatingsystem_id = data.foreman_operatingsystem.os.id
-  domain_id          = data.foreman_domain.domain.id
-  ptable_id          = data.foreman_partitiontable.ptable.id
-  medium_id          = data.foreman_media.media.id
-  name               = var.instance_name
-  hostgroup_id       = data.foreman_hostgroup.hostgroup.id
-  environment_id     = data.foreman_environment.env.id
-  enable_bmc         = false
+  architecture_id         = data.foreman_architecture.architecture.id
+  domain_id               = data.foreman_domain.domain.id
+  enable_bmc              = false
+  environment_id          = data.foreman_environment.env.id
+  hostgroup_id            = data.foreman_hostgroup.hostgroup.id
   manage_power_operations = false
-
+  model_id                = data.foreman_model.model.id
+  medium_id               = data.foreman_media.media.id
+  name                    = var.instance_name
+  operatingsystem_id      = data.foreman_operatingsystem.os.id
+  ptable_id               = data.foreman_partitiontable.ptable.id
   interfaces_attributes {
     mac       = local.mac_address
     managed   = true
-    type      = "interface"
-    provision = true
     primary   = true
+    provision = true
+    type      = "interface"
   }
 }
 
 
 data "foreman_architecture" "architecture" {
-  name = var.arch
+  name = var.architecture
 }
 
 data "foreman_domain" "domain" {
@@ -33,15 +33,19 @@ data "foreman_environment" "env" {
 }
 
 data "foreman_hostgroup" "hostgroup" {
-  title = var.hostgroup
+  title = var.foreman_hostgroup
+}
+
+data "foreman_media" "media" {
+  name = var.foreman_media_name
+}
+
+data "foreman_model" "model" {
+  name = var.foreman_model_name
 }
 
 data "foreman_operatingsystem" "os" {
   title = format("%s %s.%s", var.distro, var.os_major_version, var.os_minor_version)
-}
-
-data "foreman_media" "media" {
-  name = "RedHatCERN"
 }
 
 data "foreman_partitiontable" "ptable" {
