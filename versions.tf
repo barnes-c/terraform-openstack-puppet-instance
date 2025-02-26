03323a4938cf55f400884a54529b0ddd7627cf75
Christopher Barnes <christopher.barnes@cern.ch>
2025-02-26 16:40:24 +0100

    Add hashicop/local

--- versions.tf
+++ versions.tf
@@ -16,6 +16,10 @@ terraform {
       source  = "hashicorp/random"
       version = ">= 2.0.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.1.0"
+    }
   }
   required_version = ">= 1.0"
 }
