db03fc83e2395b5d978b0faa5e5048075a4fce4c
Christopher Barnes <christopher.barnes@cern.ch>
2025-02-27 15:39:11 +0100

    Use certmgr-stage command instead of curl

--- certmgr.tf
+++ certmgr.tf
@@ -1,12 +1,5 @@
 resource "null_resource" "certmgr_stage" {
   provisioner "local-exec" {
-    command = <<EOT
-        certmgr-stage --host ${resource.foreman_host.host.interfaces_attributes[0].name} -d 
-    EOT
+    command = "certmgr-stage --host ${resource.foreman_host.host.interfaces_attributes[0].name} -d" 
   }
-}
-
-output "certmgr_staging_status" {
-  description = "Indicates if the host has been staged in Certmgr"
-  value       = "Staging request sent for ${resource.foreman_host.host.interfaces_attributes[0].name}"
 }
