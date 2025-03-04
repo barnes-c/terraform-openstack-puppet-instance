7e7a7a1219b740d7766e8b94b8741fd15d2b1e4a
Christopher Barnes <christopher.barnes@cern.ch>
2025-03-04 09:51:20 +0100

    Move module to root

--- examples/multiple-availabilty-zones/main.tf
+++ examples/multiple-availabilty-zones/main.tf
@@ -1,5 +1,5 @@
 module "terraform-openstack-instance" {
-  source = "git::https://gitlab.cern.ch/ai-config-team/terraform-puppet/terraform-openstack-puppet-instance.git?ref=1.0.2"
+  source = "git::https://gitlab.cern.ch/ai-config-team/terraform-puppet/terraform-openstack-puppet-instance.git?ref=1.0.4"
   
   count  = length(var.availability_zones)
 
--- main.tf
+++
@@ -1,5 +0,0 @@
-module "terraform-openstack-instance" {
-  source = "./module"
-
-  key_pair_name     = var.key_pair_name
-}
