714c10369c06148a67bd894e514870eaa88820a2
Christopher Barnes <christopher.barnes@cern.ch>
2025-03-04 12:33:58 +0100

    Update versions

--- examples/multiple-availabilty-zones/main.tf
+++ examples/multiple-availabilty-zones/main.tf
@@ -1,10 +1,10 @@
 module "terraform-openstack-instance" {
-  source = "git::https://gitlab.cern.ch/ai-config-team/terraform-puppet/terraform-openstack-puppet-instance.git?ref=1.0.5"
+  source = "git::https://gitlab.cern.ch/ai-config-team/terraform-puppet/terraform-openstack-puppet-instance.git?ref=1.0.6"
   
   count  = length(var.availability_zones)
 
   key_pair_name     = var.key_pair_name
   availability_zone = var.availability_zones[count.index]
-  instance_name     = "terraform-openstack-instance-0${count.index}"
-  volume_name       = "terraform-openstack-instance-0${count.index}"
+  instance_name     = "terraform-openstack-puppet-instance-0${count.index}"
+  volume_name       = "terraform-openstack-puppet-instance-0${count.index}"
 }
--- examples/single-instance/main.tf
+++ examples/single-instance/main.tf
@@ -1,5 +1,5 @@
 module "terraform-openstack-instance" {
-  source = "git::https://gitlab.cern.ch/ai-config-team/terraform-puppet/terraform-openstack-puppet-instance.git?ref=1.0.2"
+  source = "git::https://gitlab.cern.ch/ai-config-team/terraform-puppet/terraform-openstack-puppet-instance.git?ref=1.0.6"
 
   key_pair_name = var.key_pair_name
 }
