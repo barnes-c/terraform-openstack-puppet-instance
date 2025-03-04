b1789050e5afe118e26a59d456fe3c0e12248451
Christopher Barnes <christopher.barnes@cern.ch>
2025-03-04 09:21:46 +0100

    Remove module local provider cfg

--- examples/multiple-availabilty-zones/main.tf
+++ examples/multiple-availabilty-zones/main.tf
@@ -3,8 +3,8 @@ module "terraform-openstack-instance" {
   
   count  = length(var.availability_zones)
 
-  instance_name     = "terraform-openstack-instance-1${count.index}"
-  volume_name       = "terraform-openstack-instance-1${count.index}"
   key_pair_name     = var.key_pair_name
   availability_zone = var.availability_zones[count.index]
+  instance_name     = "terraform-openstack-instance-0${count.index}"
+  volume_name       = "terraform-openstack-instance-0${count.index}"
 }
