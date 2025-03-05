84eb2eb13b3bb19eb9359cc7097633ea19fa9fec
Christopher Barnes <christopher.barnes@cern.ch>
2025-03-05 11:26:51 +0100

    Update module source

--- examples/single-instance/main.tf
+++ examples/single-instance/main.tf
@@ -1,5 +1,5 @@
 module "terraform-openstack-instance" {
-  source = "git::https://gitlab.cern.ch/ai-config-team/terraform-puppet/terraform-openstack-puppet-instance.git?ref=1.0.7"
+  source = "git::https://gitlab.cern.ch/ai-config-team/terraform-puppet/terraform-openstack-puppet-instance.git?ref=1.0.8"
 
   key_pair_name = var.key_pair_name
 }
