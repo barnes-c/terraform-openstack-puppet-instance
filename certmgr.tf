8174ba9dcb53f1c968313220de0f1e26369ef9ab
Christopher Barnes <christopher.barnes@cern.ch>
2025-03-06 11:28:45 +0100

    Add kerberos authentication to cert curl

--- certmgr.tf
+++ certmgr.tf
@@ -1,7 +1,9 @@
-resource "null_resource" "certmgr_stage" {
+resource "null_resource" "certmgr" {
   provisioner "local-exec" {
     command = <<EOT
-      curl -X POST \
+      curl -s --negotiate -u : \
+        -X POST \
+        -w "\nHTTP_STATUS:%%{http_code}\n" \
         -H "Content-Type: application/json" \
         -d '{"hostname": "${resource.foreman_host.host.interfaces_attributes[0].name}"}' \
         "https://${var.certmgr_host}:${var.certmgr_port}/krb/certmgr/staged/"
