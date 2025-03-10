287a267f08d74c909f5748162f13a6f61e910e8c
Christopher Barnes <christopher.barnes@cern.ch>
2025-03-10 10:47:29 +0100

    Start extensive-example

---
+++ examples/extensive-example/variables.tf
@@ -0,0 +1,22 @@
+variable "foreman_hostname" {
+  description = "The hostname of the foreman server"
+  type        = string
+  default     = "judy.cern.ch"
+}
+
+variable "key_pair_name" {
+  description = "Name of the key pair"
+  type        = string
+}
+
+variable "location_id" {
+  description = "The foreman location ID"
+  type        = number
+  default     = 1
+}
+
+variable "organization_id" {
+  description = "The foreman organization ID"
+  type        = number
+  default     = 2
+}
---
+++ examples/extensive-example/versions.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_providers {
+    foreman = {
+      source  = "terraform-coop/foreman"
+      version = "0.7.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
