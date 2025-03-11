bec8c06c22fb898ae6d24cc0fa6a988ef74dcb1a
Christopher Barnes <christopher.barnes@cern.ch>
2025-03-11 13:32:03 +0100

    Update module source version

--- examples/multiple-availabilty-zones/main.tf
+++ examples/multiple-availabilty-zones/main.tf
@@ -15,7 +15,7 @@ locals {
 }
 
 module "terraform-openstack-instance" {
-  source = "git::https://gitlab.cern.ch/ai-config-team/terraform-puppet/terraform-openstack-puppet-instance.git?ref=1.0.11"
+  source = "git::https://gitlab.cern.ch/ai-config-team/terraform-puppet/terraform-openstack-puppet-instance.git?ref=1.0.12"
   count  = length(local.limited_availability_zones)
 
   key_pair_name     = var.key_pair_name
