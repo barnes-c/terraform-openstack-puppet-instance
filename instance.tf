3a4c78d74e5cddd8ff3f6f23cebf48571a94fae4
Christopher Barnes <christopher.barnes@cern.ch>
2025-03-20 09:16:43 +0100

    Ignore changes to user_data

--- instance.tf
+++ instance.tf
@@ -11,10 +11,15 @@ resource "openstack_compute_instance_v2" "instance" {
 
   metadata = merge(
     {
+      cern-waitdns = true
       tenant-id    = data.openstack_identity_auth_scope_v3.scope.project_id
       tenant-name  = data.openstack_identity_auth_scope_v3.scope.project_name
     },
     var.landb_mainuser != "" ? { "landb-mainuser" = var.landb_mainuser } : {},
     var.landb_responsible != "" ? { "landb-responsible" = var.landb_responsible } : {}
   )
+
+  lifecycle {
+    ignore_changes = [user_data]
+  }
 }
