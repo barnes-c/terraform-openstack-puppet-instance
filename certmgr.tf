aa193b1a5f687ffc37f3a0bf83f2d1c65a943f11
Christopher Barnes <christopher.barnes@cern.ch>
2025-03-20 09:07:17 +0100

    Resolve hector to baby

--- certmgr.tf
+++ certmgr.tf
@@ -2,12 +2,17 @@ resource "null_resource" "certmgr" {
   provisioner "local-exec" {
     when    = create
     command = <<EOT
+      # Make hector sticky (use one specific baby)
+      FQDN=$(python3 -c "import socket; print(socket.getfqdn('${var.certmgr_host}'))")
+
+      echo "Resolved FQDN: $FQDN"
+
       curl -s --negotiate -u : \
         -X POST \
-        -w "\nHTTP_STATUS:%%{http_code}\n" \
+        -w "\\nHTTP_STATUS:%%{http_code}\\n" \
         -H "Content-Type: application/json" \
         -d '{"hostname": "${resource.foreman_host.host.interfaces_attributes[0].name}"}' \
-        "https://${var.certmgr_host}:${var.certmgr_port}/krb/certmgr/staged/"
+        "https://$FQDN:${var.certmgr_port}/krb/certmgr/staged/"
     EOT
   }
 }
