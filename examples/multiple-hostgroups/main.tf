86f4a731036673fbe44df5e2ff9e3a55fb2385c0
Christopher Barnes <christopher.barnes@cern.ch>
2025-04-11 10:43:32 +0200

    Bump module version

--- examples/multiple-hostgroups/main.tf
+++ examples/multiple-hostgroups/main.tf
@@ -1,12 +1,12 @@
 module "hostgroup-1" {
-  source = "git::https://gitlab.cern.ch/ai-config-team/terraform-puppet/terraform-openstack-puppet-instance.git?ref=1.0.19"
+  source = "git::https://gitlab.cern.ch/ai-config-team/terraform-puppet/terraform-openstack-puppet-instance.git?ref=1.0.20"
 
   foreman_hostgroup = "playground/chbarnes"
   instance_name     = "test-${replace("playground/chbarnes", "/", "-")}-1"
 }
 
 module "hostgroup-2" {
-  source = "git::https://gitlab.cern.ch/ai-config-team/terraform-puppet/terraform-openstack-puppet-instance.git?ref=1.0.19"
+  source = "git::https://gitlab.cern.ch/ai-config-team/terraform-puppet/terraform-openstack-puppet-instance.git?ref=1.0.20"
 
   foreman_hostgroup = "playground"
   instance_name     = "test-${replace("playground", "/", "-")}-1"
--- examples/simple-instance/main.tf
+++ examples/simple-instance/main.tf
@@ -1,3 +1,3 @@
 module "terraform-openstack-instance" {
-  source = "git::https://gitlab.cern.ch/ai-config-team/terraform-puppet/terraform-openstack-puppet-instance.git?ref=1.0.19"
+  source = "git::https://gitlab.cern.ch/ai-config-team/terraform-puppet/terraform-openstack-puppet-instance.git?ref=1.0.20"
 }
