6b921e59fff49a4d6c1788c1d46de39e929c4a98
Christopher Barnes <christopher.barnes@cern.ch>
2025-05-06 15:51:46 +0200

    Bump certMgr

--- modules/instance/versions.tf
+++ modules/instance/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     certmgr = {
       source  = "barnes-c/certmgr"
-      version = "1.2.1"
+      version = "1.3.0"
     }
     foreman = {
       source  = "terraform-coop/foreman"
