19d67cae4a73534fd83a32e51fcb251679b387d6
Christopher Barnes <christopher.barnes@cern.ch>
2025-05-07 09:46:23 +0200

    Update versions of examples

--- examples/simple-instance/versions.tf
+++ examples/simple-instance/versions.tf
@@ -1,16 +1,20 @@
 terraform {
   required_providers {
     certmgr = {
-      source = "barnes-c/certmgr"
-      version = "1.2.1"
+      source  = "barnes-c/certmgr"
+      version = "1.3.0"
     }
     foreman = {
       source  = "terraform-coop/foreman"
       version = "0.7.0"
     }
+    landb = {
+      source  = "barnes-c/landb"
+      version = ">= 1.0.12"
+    }
     roger = {
-      source = "barnes-c/roger"
-      version = "1.2.2"
+      source  = "barnes-c/roger"
+      version = "1.2.3"
     }
   }
   required_version = ">= 1.0"
